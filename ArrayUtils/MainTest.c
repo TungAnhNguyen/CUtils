e442bb909a34800fe9519275d38f9972ae95fd44
Nguyen Tung Anh <tnguyen2@iwu.edu>
2020-04-19 19:55:12 +0700

    remove not used BinarySearch.c || Add new file MainTest.c for scrambling test.

---
+++ ArrayUtils/MainTest.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "ArrayUtils.c"
+
+int main()
+{
+	int sortArray[] = {69, 12, 7, 8, 12, 11, 4, 6, 100, 10, 2, 5, 13, 18, 21, 3};
+	//sorted: 		   2; 3; 4; 5; 6; 7; 8; 10; 11; 12; 12; 13; 18; 21; 69, 100;
+	int size  = sizeof(sortArray)/sizeof(int);
+	printf("size: %d\n", size);
+	int* sortPointer = sortArray;
+	bubbleSort(sortPointer, size);
+
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d; ", sortArray[i]);
+	}
+	printf("\n");
+
+	int numberToFind = 21;
+	int foundIndex = binarySearch(sortArray, numberToFind, size);
+	printf("foundIndex: %d\n", foundIndex);
+}
